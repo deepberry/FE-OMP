1b601dd478c8825f0f10373ea97dbb77ad7f772a
iRuxu <17229398@qq.com>
2022-06-24 19:23:24 +0800

    fix:cdn & router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 /*
  * @Author: iRuxu
  * @Date: 2022-06-20 17:26:26
- * @LastEditTime: 2022-06-20 19:01:07
+ * @LastEditTime: 2022-06-24 19:17:02
  * @Description:
  */
 // 1.Dependency
 import {
     createRouter,
-    // createWebHistory,
-    createWebHashHistory,
+    createWebHistory,
+    // createWebHashHistory,
 } from "vue-router";
 
 // 2.Components
@@ -48,9 +48,9 @@ const routes = [
 
 // 4.Build An Instance
 const router = createRouter({
-    history: createWebHashHistory(), //hash
-    // history: createWebHistory(),  //history api
-    // base : '/rewrite root',
+    // history: createWebHashHistory(), //hash
+    history: createWebHistory(), //history api
+    base: "/omp",
     routes,
 });
 
