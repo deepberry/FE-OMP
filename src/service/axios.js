5cd749bc8885600171db2289e20d20d16b658bbc
litthy <litthy@qq.com>
2022-08-19 14:34:19 +0800

    fix: 拦截返回msg

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,53 +1,22 @@
 import axios from "axios";
 import qs from "qs";
-import { ElMessage, ElMessageBox, ElNotification } from "element-plus";
-function loadPop(msg, popType = "message") {
-    switch (popType) {
-        case "message":
-            ElMessage({
-                type: "error",
-                message: msg,
-            });
-            break;
-        case "alert":
-            ElMessageBox.confirm(msg, "错误", {
-                confirmButtonText: "确定",
-                type: "warning",
-            });
-            break;
-        case "notify":
-            ElNotification({
-                title: "错误",
-                message: msg,
-                type: "error",
-            });
-            break;
-        default:
-            break;
-    }
-}
+import { ElMessage } from "element-plus";
+
 function installCmsInterceptors(target, options) {
-    let popType = (options && options.popType) || "message";
-    target["interceptors"]["response"].use(
-        function (response) {
-            if (response.data.code) {
-                if (!options || !options.mute) {
-                    response.data.msg && loadPop(`[${response.data.code}]${response.data.msg}`, popType);
-                }
-                return Promise.reject(response);
-            }
-            return response;
+    options;
+    target.interceptors.response.use(
+        (res) => {
+            return res;
         },
-        function (err) {
-            if (!options || !options.mute) {
-                loadPop(`[${err.response.data.statusCode}]${err.response.data.message}`, popType);
-            }
-            return PopNextworkError(err);
+        (err) => {
+            if (err.response.data)
+                ElMessage({
+                    type: "error",
+                    message: "code:" + err.response.data.httpCode + "=>" + err.response.data.msg,
+                });
+            return Promise.reject(err);
         }
     );
-}
-function PopNextworkError(err) {
-    return Promise.reject(err);
 }
 
 function $admin(options) {
