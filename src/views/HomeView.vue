82664978a134ac28530aa90e06fa54b5fcd1392a
litthy <litthy@qq.com>
2022-08-31 10:33:14 +0800

    fix: 地址添加哈希

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
 
 // 跳转链接数据
 const APPID = "ww5429d07e97752284";
-const REDIRECT_URI = encodeURIComponent("https://admin.deepberry.cn/omp/role/details/0");
+const REDIRECT_URI = encodeURIComponent("https://admin.deepberry.cn/omp/#/role/details/0");
 const path = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
 
 window.location.href = path;
