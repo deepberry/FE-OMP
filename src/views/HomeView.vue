e684b63d4a9ea7c780b2a984bfabcf25f63f0d70
litthy <litthy@qq.com>
2022-10-27 15:43:48 +0800

    fix: disabled

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ const APPID = "ww5429d07e97752284";
 const REDIRECT_URI = encodeURIComponent("https://admin.deepberry.cn/omp/#/role/details/0");
 const path = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`;
 
-window.location.href = path;
+window.location.href = process.env.NODE_ENV === "production" ? path : "https://admin.deepberry.cn/omp/#/role/details/0";
 </script>
 <style lang="less" scoped>
 @import "@/assets/css/views/home.less";
